00b2989c2182a9b6ab0e77cfa110a6eeb291b0dd
ArielGuevara <adguevara7@espe.edu.ec>
2025-04-09 23:45:21 -0500

    Add overlay to menu toggle for improved user experience

--- src/lib/types/layout.ts
+++ src/lib/types/layout.ts
@@ -23,13 +23,19 @@ document.addEventListener('DOMContentLoaded', () => {
 export function setupMenuToggle() {
     const menu = document.getElementById('main-menu');
     const container_nav = document.getElementById('main-menu-nav');
+    const overlay = document.createElement('div');
 
     if (!menu || !container_nav) return;
     container_nav.classList.add('hidden', 'translate-x-full');
 
+    overlay.id = 'overlay';
+    overlay.classList.add('fixed', 'inset-0','backdrop-blur-sm', 'z-10', 'hidden');
+    document.body.appendChild(overlay);
+
     menu.addEventListener('click', () => {
         container_nav.classList.remove('hidden');
         void container_nav.offsetWidth;
+        overlay.classList.remove('hidden');
         container_nav.classList.remove('translate-x-full');
         container_nav.classList.add('translate-x-0');
     })
@@ -38,10 +44,13 @@ export function setupMenuToggle() {
 export function closeMenu() {
     const nav_x = document.getElementById('menu-nav-x');
     const container_nav = document.getElementById('main-menu-nav');
-    if (!nav_x || !container_nav)  return;
+    const overlay = document.getElementById('overlay');
+
+    if (!nav_x || !container_nav || !overlay)  return;
     nav_x.addEventListener('click', () => {
         container_nav.classList.remove('translate-x-0');
         container_nav.classList.add('translate-x-full');
+        overlay.classList.add('hidden');
 
         container_nav.addEventListener('transitionend', () => {
             if (container_nav.classList.contains('translate-x-full')) {
